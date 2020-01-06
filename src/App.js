4eb8802a1ba8864d8f2988397fdc0e334e40f484
Luis Laborda <luis.laborda@mckesson.com>
2020-01-06 13:56:25 -0600

    Iterates through items

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import Cartfooter from './components/cart_footer/CartFooter';
 import CartItems from './components/cart_items/CartItems';
 
 function App() {
+
+  const cartItemsList = [
+    { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+    { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+    { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+  ];
+
   return (
     <div className="App">
       <Cartheader />
-      <CartItems />
+      <CartItems items={cartItemsList} />
       <Cartfooter copyright="2020" />
     </div>
   );
--- src/components/cart_items/CartItems.js
+++ src/components/cart_items/CartItems.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Item from './CartItem';
 
-const CartItems = () => {
+const CartItems = ({items}) => {
+
+    let itemList = items.map(item => <Item key={ item.id } product={ item.product } quantity={item.quantity} />)
+
     return (
         <div className="container">
             <h1>Items</h1>
+            <div className="list-group">
+                <div className="list-group-item">
+                <div className="row">
+                    <div className="col-md-8">Product</div>
+                    <div className="col-md-2">Price</div>
+                    <div className="col-md-2">Quantity</div>
+                </div>
+                </div>
+                
+                { itemList }
+            </div>
         </div>
     )
 }
